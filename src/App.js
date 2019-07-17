c0bf7bc1a4a6ac1147dfff5063e8b318e3852475
turisap <kirillshakirov57@gmail.com>
2019-07-17 05:10:12 -0400

    Initials for ErrorBoundary, home page, sign up form. Enabled relative imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
 import './App.css';
+import ErrorBoundary from 'base/ErrorBoundary';
 
 const Home = lazy(() => import('./routes/Home'));
 const SignUpForm = lazy(() => import('./routes/SignUpForm'));
+const NotFoundPage = lazy(() => import('./routes/404'));
 
 // TODO add nprogress on route change
 // TODO Code splitting with dynamic imports in router (https://serverless-stack.com/chapters/code-splitting-in-create-react-app.html)
 // TODO Add package to run evn varibales from .env
+// TODO Error boundaries
+// TODO on page load animation like that one on gitlab
+// TODO add loading component
+
+// TODO MUST DO ALL TESTS STRAIGHT AFTER CREATING A FORM
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ErrorBoundary>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/signup" component={SignUpForm} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
---
+++ src/routes/SignUpForm.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SignUpPage = () => <h1>SignUpPage</h1>;
+
+export default SignUpPage;
