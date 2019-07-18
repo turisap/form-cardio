d17044ea2dfc5cd1052ccf7cd10d356d1325f0d4
turisap <kirillshakirov57@gmail.com>
2019-07-18 02:45:49 -0400

    Buggy loading animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
-import './App.css';
 import ErrorBoundary from 'base/ErrorBoundary';
-import Nprogress from 'routes/Nprogress';
+import Nprogress from 'base/Nprogress';
+import Loading from 'base/Loading';
+import './App.css';
 
 const Home = lazy(() => import('./routes/Home'));
 const SignUpForm = lazy(() => import('./routes/SignUpForm'));
 const NotFoundPage = lazy(() => import('./routes/404'));
 
-// TODO add nprogress on route change
-// TODO Code splitting with dynamic imports in router (https://serverless-stack.com/chapters/code-splitting-in-create-react-app.html)
-// TODO Add package to run evn varibales from .env
 // TODO on page load animation like that one on gitlab
 // TODO add loading component
+// TODO add Google Analitics to count visitors
+// TODO Portals
+// TODO try-catch in event listeners
 
 // TODO MUST DO ALL TESTS STRAIGHT AFTER CREATING A FORM
 function App() {
   return (
     <ErrorBoundary>
       <Router>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={Loading}>
           <Nprogress>
             <Switch>
               <Route exact path="/" component={Home} />
--- src/base/ErrorBoundary.js
+++ src/base/ErrorBoundary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// TODO to finish
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
