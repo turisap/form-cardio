09802001a31c2ba67e74ad3e3638a6b1cee46470
turisap <kirillshakirov57@gmail.com>
2019-08-20 05:59:08 -0400

    adjacements to homepage styling

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -21,8 +21,13 @@ const StyledHomePage = styled.div`
   .home-page__inner {
     margin-top: 80px;
     display: grid;
-    grid-template-rows: 180px 170px 100px;
+    grid-template-rows: 180px 190px 100px;
     grid-template-columns: 500px;
+  }
+
+  .home-page__section {
+    display: grid;
+    align-content: center;
   }
 
   .home-page__contents {
@@ -59,90 +64,114 @@ const HomePage = () => {
     <StyledHomePage className="container">
       <div className="home-page__inner">
         <div className="home-page__what">
-          {loadingSequence < 2 ? (
-            <Placeholder>
-              <Placeholder.Header>
-                <Placeholder.Line length="medium" />
-              </Placeholder.Header>
-              <Placeholder.Paragraph>
-                <Placeholder.Line length="very long" />
-                <Placeholder.Line length="very long" />
-                <Placeholder.Line length="very long" />
-                <Placeholder.Line length="very long" />
-                <Placeholder.Line length="very long" />
-              </Placeholder.Paragraph>
-            </Placeholder>
-          ) : (
-            <>
-              <h2>What is this</h2>
-              <div className="home-page__contents">
-                <p>
-                  This is a set of different web forms which I created to
-                  practice CSS grid for creating forms
-                </p>
-                <p>
-                  It was inspired after struggling with the forms in previous
-                  application
-                </p>
-              </div>
-            </>
-          )}
+          <div className="home-page__section">
+            {loadingSequence < 4 ? (
+              <Placeholder>
+                <Placeholder.Header>
+                  <Placeholder.Line length="medium" />
+                </Placeholder.Header>
+                <Placeholder.Paragraph>
+                  <Placeholder.Line length="very long" />
+                  <Placeholder.Line length="very long" />
+                  <Placeholder.Line length="very long" />
+                  <Placeholder.Line length="very long" />
+                </Placeholder.Paragraph>
+              </Placeholder>
+            ) : (
+              <>
+                <h2>What is this</h2>
+                <div className="home-page__contents">
+                  <p>
+                    This is a set of different web forms which I created to
+                    practice CSS grid for creating forms
+                  </p>
+                  <p>
+                    It was inspired after struggling with the forms in previous
+                    application
+                  </p>
+                </div>
+              </>
+            )}
+          </div>
         </div>
         <div className="home-page__stack">
-          {loadingSequence < 5 ? (
-            <Placeholder>
-              <Placeholder.Header>
-                <Placeholder.Line length="medium" />
-              </Placeholder.Header>
-              <Placeholder.Paragraph>
-                <Placeholder.Line length="short" />
-                <Placeholder.Line length="short" />
-                <Placeholder.Line length="short" />
-                <Placeholder.Line length="short" />
-              </Placeholder.Paragraph>
-            </Placeholder>
-          ) : (
-            <>
-              <h2>Stack being used:</h2>
-              <ul>
-                <li>
-                  <p className="home-page__stackelement">create-react-app</p>
-                </li>
-                <li>
-                  <p className="home-page__stackelement">styled-components</p>
-                </li>
-                <li>
-                  <p className="home-page__stackelement">react-advanced-form</p>
-                </li>
-                <li>
-                  <p className="home-page__stackelement">semantic-ui-react</p>
-                </li>
-              </ul>
-            </>
-          )}
+          <div className="home-page__section">
+            {loadingSequence < 9 ? (
+              <Placeholder>
+                <Placeholder.Header>
+                  <Placeholder.Line length="medium" />
+                </Placeholder.Header>
+                <Placeholder.Paragraph>
+                  <Placeholder.Line length="medium" />
+                  <Placeholder.Line length="medium" />
+                  <Placeholder.Line length="long" />
+                  <Placeholder.Line length="very long" />
+                </Placeholder.Paragraph>
+              </Placeholder>
+            ) : (
+              <>
+                <h2>Stack being used:</h2>
+                <ul>
+                  <li>
+                    <p>
+                      <span className="home-page__stackelement">
+                        create-react-app
+                      </span>
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="home-page__stackelement">
+                        tyled-components
+                      </span>{' '}
+                      for styling
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="home-page__stackelement">
+                        react-advanced-form
+                      </span>{' '}
+                      for form validation
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <span className="home-page__stackelement">
+                        semantic-ui-react
+                      </span>{' '}
+                      for using such elements as placeholders
+                    </p>
+                  </li>
+                </ul>
+              </>
+            )}
+          </div>
         </div>
         <div className="home-page__features">
-          {loadingSequence < 7 ? (
-            <Placeholder>
-              <Placeholder.Header>
-                <Placeholder.Line length="medium" />
-              </Placeholder.Header>
-              <Placeholder.Paragraph>
-                <Placeholder.Line length="short" />
-                <Placeholder.Line length="short" />
-                <Placeholder.Line length="short" />
-                <Placeholder.Line length="short" />
-              </Placeholder.Paragraph>
-            </Placeholder>
-          ) : (
-            <>
-              <h2>New things tried:</h2>
-              <ul>
-                <li>React Hooks</li>
-                <li>React Portals</li>
-              </ul>
-            </>
-          )}
+          <div className="home-page__section">
+            {loadingSequence < 11 ? (
+              <Placeholder>
+                <Placeholder.Header>
+                  <Placeholder.Line length="medium" />
+                </Placeholder.Header>
+                <Placeholder.Paragraph>
+                  <Placeholder.Line length="short" />
+                  <Placeholder.Line length="short" />
+                  <Placeholder.Line length="short" />
+                  <Placeholder.Line length="short" />
+                </Placeholder.Paragraph>
+              </Placeholder>
+            ) : (
+              <>
+                <h2>New things tried:</h2>
+                <ul>
+                  <li>React Hooks</li>
+                  <li>React Portals</li>
+                </ul>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </StyledHomePage>
